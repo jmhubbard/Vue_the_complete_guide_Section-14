b708dec7cd0d767038a1536eb3edc964454887ee
Jason Hubbard <jasonhubb@gmail.com>
2020-11-20 17:49:32 -0800

    add animation to baseModal when the popup appears on screen

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -34,5 +34,18 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
+  animation: modal 0.3s ease-out forwards;
+}
+
+@keyframes modal {
+  0% {
+    opacity: 0;
+    transform: translateY(-50px) scale(0.9);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
 }
 </style>
