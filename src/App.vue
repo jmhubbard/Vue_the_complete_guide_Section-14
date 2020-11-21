460bdd3e21fdce2503d36d7733d3f0c1ae0d82a5
Jason Hubbard <jasonhubb@gmail.com>
2020-11-20 19:53:19 -0800

    add name to transition tag and rename default classes

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
@@ -90,10 +90,10 @@ button:active {
 
 .animate {
   /* transform: translateX(-150px); */
-  animation: slide-fade 0.3s ease-out forwards;
+  animation: slide-scale 0.3s ease-out forwards;
 }
 
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   }
@@ -107,30 +107,30 @@ button:active {
   }
 }
 
-.v-enter-from {
+.para-enter-from {
   opacity: 0;
   transform: translateY(-30px);
 }
-.v-enter-active {
+.para-enter-active {
   transition: all 0.3s ease-out;
 }
 
-.v-enter-to {
+.para-enter-to {
   opacity: 1;
   transform: translateY(0);
 }
 
-.v-leave-from {
+.para-leave-from {
   opacity: 1;
   transform: translateY(0);
 }
 
-.v-leave-active {
+.para-leave-active {
     transition: all 0.3s ease-in;
 
 }
 
-.v-leave-to {
+.para-leave-to {
   opacity: 0;
   transform: translateY(-30px);
 
