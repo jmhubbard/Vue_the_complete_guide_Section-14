c1101fd902f98a1808296c0b925202bf83495bda
Jason Hubbard <jasonhubb@gmail.com>
2020-11-20 20:23:50 -0800

    add transition animation inside modal

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
-    <p>This is a test dialog!</p>
-    <button @click="hideDialog">Close it!</button>
-  </base-modal>
+    <base-modal @close="hideDialog" :open="dialogIsVisible">
+      <p>This is a test dialog!</p>
+      <button @click="hideDialog">Close it!</button>
+    </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="backdrop" @click="$emit('close')"></div>
-  <dialog open>
-    <slot></slot>
-  </dialog>
+  <div v-if="open" class="backdrop" @click="$emit('close')"></div>
+  <transition name="modal">
+    <dialog open v-if="open">
+      <slot></slot>
+    </dialog>
+  </transition>
 </template>
 
 <script>
 export default {
+  props: ['open'],
   emits: ['close'],
 };
 </script>
@@ -34,7 +37,7 @@ dialog {
   background-color: white;
   z-index: 100;
   border: none;
-  animation: modal 0.3s ease-out forwards;
+  /* animation: modal 0.3s ease-out forwards; */
 }
 
 @keyframes modal {
@@ -48,4 +51,33 @@ dialog {
     transform: translateY(0) scale(1);
   }
 }
+
+.modal-enter-from {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+.modal-enter-active {
+  animation: modal 0.3s ease-out;
+}
+
+.modal-enter-to {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.modal-leave-from {
+  /* opacity: 1;
+  transform: translateY(0); */
+}
+
+.modal-leave-active {
+  animation: modal 0.3s ease-in reverse;
+
+}
+
+.modal-leave-to {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
+
 </style>
